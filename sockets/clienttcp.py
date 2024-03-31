4effe636ec37e711c0e915e1be6c5bc268848c9b
Henri Mattos <145288353+HenriMattos@users.noreply.github.com>
2024-03-30 21:13:42 -0300

    clienttcp.py

--- sockets/clienttcp.py
+++ sockets/clienttcp.py
@@ -1,17 +1,27 @@
 import socket
 
+username = input("Digite seu nome de usuário: ")
+
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 try:
     while True:
-        message = input("você: ") + "\n"
-        client.sendto(message.encode(), ("127.0.0.1", 666))
+        message = input(f"{username}: ") + "\n"
+        client.sendto(message.encode(), ("127.0.0.1", 666)) #o localhost e a porta é só um exemplo.
         msg, friend = client.recvfrom(1024)
         print(f"{friend[0]}: {msg.decode()}")
 
+except ConnectionError:
+    print("Erro ao conectar ao servidor.")
+except TimeoutError:
+    print("Tempo limite de espera esgotado.")
+except ValueError:
+    print("Mensagem inválida.")
+except PermissionError:
+    print("Acesso não autorizado.")
 except Exception as error:
-    print("conexão falhou")
-    print(error)
+    print("Erro inesperado:", error)
 
 finally:
     client.close()
+
